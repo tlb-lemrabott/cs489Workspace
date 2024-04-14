c9868176afd6c48910b3e566912bfa7e695b1d50
Lemrabott <llemrabott52@gmail.com>
2024-04-14 07:25:18 -0500

    initate ADS REST application

---
+++ AdsRESTApplication/src/test/java/miu/edu/cs489/adsrestapplication/AdsRestApplicationTests.java
@@ -0,0 +1,13 @@
+package miu.edu.cs489.adsrestapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdsRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
